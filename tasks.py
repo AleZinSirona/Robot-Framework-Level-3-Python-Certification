8b3bfd5e0324d15f0f6c78164ccebdc208ce36c8
Alexander Zinn Prüfkammer <alexander.zinn@dentsplysirona.com>
2024-06-10 09:17:21 +0200

    added a new lines

--- tasks.py
+++ tasks.py
@@ -1,4 +1,7 @@
 from robocorp.tasks import task
+from RPA.HTTP import HTTP
+
+http = HTTP()
 
 @task
 def produce_traffic_data():
@@ -7,6 +10,7 @@ def produce_traffic_data():
     Produces traffic data work items.
     """
     print("produce")
+    download_traffic_data()
 
 
 @task
@@ -16,3 +20,10 @@ def consume_traffic_data():
     Consumes traffic data work items.
     """
     print("consume")
+
+def download_traffic_data():
+    http.download(
+        url="https://github.com/robocorp/inhuman-insurance-inc/raw/main/RS_198.json",
+        target_file="output/traffic.json",
+        overwrite=True,
+    )
