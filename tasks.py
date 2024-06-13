3119c17460ee4e4b9f7f4581e55d83698a1292bd
Zinn, Alexander <Alexander.Zinn@dentsplysirona.com>
2024-06-13 07:24:59 +0200

    add new lines of code

--- tasks.py
+++ tasks.py
@@ -1,3 +1,5 @@
+import requests
+from robocorp import workitems
 from robocorp.tasks import task
 from RPA.HTTP import HTTP
 from RPA.JSON import JSON
@@ -8,6 +10,13 @@ json = JSON()
 table = Tables()
 
 TRAFFIC_JSON_FILE_PATH = "output/traffic.json"
+
+# JSON data keys
+COUNTRY_KEY = "SpatialDim"
+YEAR_KEY = "TimeDim"
+RATE_KEY = "NumericValue"
+GENDER_KEY = "Dim1"
+
 @task
 def produce_traffic_data():
     """
@@ -21,6 +30,12 @@ def produce_traffic_data():
     )
     traffic_data = load_traffic_data_as_table()
     table.write_table_to_csv(traffic_data, "output/test.csv")
+    filtered_data = filter_and_sort_traffic_data(traffic_data)
+    filtered_data = get_latest_data_by_country(filtered_data)
+    payloads = create_work_item_payloads(filtered_data)
+    save_work_item_payloads(payloads)
+
+
 
 @task
 def consume_traffic_data():
@@ -28,8 +43,62 @@ def consume_traffic_data():
     Inhuman Insurance, Inc. Artificial Intelligence System automation.
     Consumes traffic data work items.
     """
-    print("consume")
+    process_traffic_data()
+
+
 
 def load_traffic_data_as_table():
     json_data = json.load_json_from_file(TRAFFIC_JSON_FILE_PATH)
     return table.create_table(json_data["value"])
+
+def filter_and_sort_traffic_data(data):
+    max_rate = 5.0
+    both_genders = "BTSX"
+    table.filter_table_by_column(data, RATE_KEY, "<", max_rate)
+    table.filter_table_by_column(data, GENDER_KEY, "==", both_genders)
+    table.sort_table_by_column(data, YEAR_KEY, False)
+    return data
+
+def get_latest_data_by_country(data):
+
+    data = table.group_table_by_column(data, COUNTRY_KEY)
+    latest_data_by_country = []
+    for group in data:
+        first_row = table.pop_table_row(group)
+        latest_data_by_country.append(first_row)
+    return latest_data_by_country
+
+def create_work_item_payloads(traffic_data):
+    payloads = []
+    for row in traffic_data:
+        payload = dict(
+            country=row[COUNTRY_KEY],
+            year=row[YEAR_KEY],
+            rate=row[RATE_KEY],
+        )
+        payloads.append(payload)
+    return payloads
+
+def save_work_item_payloads(payloads):
+    for payload in payloads:
+        variables = dict(traffic_data=payload)
+        workitems.outputs.create(variables)
+
+def process_traffic_data():
+    for item in workitems.inputs:
+        traffic_data = item.payload["traffic_data"]
+        if len(traffic_data["country"]) == 3:
+            status, return_json = post_traffic_data_to_sales_system(traffic_data)
+            if status == 200:
+                item.done() 
+            else: 
+                item.fail(
+                    exception_type="APPLICATION",
+                    code="TRAFFIC_DATA_POST_FAILED",
+                    message=return_json["message"],
+                )
+    
+def post_traffic_data_to_sales_system(traffic_data):
+    url = "https://robocorp.com/inhuman-insurance-inc/sales-system-api"
+    response = requests.post(url, json=traffic_data)
+    return response.status_code, response.json()
